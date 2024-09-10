9acc5bae7d8767aef207fd78756ee8ade65234dc
Mayank Badal <144877956+mankuBadal24@users.noreply.github.com>
2024-09-10 23:53:17 +0530

    first

---
+++ SpringProject/src/in/sp/main/Main.java
@@ -0,0 +1,15 @@
+package in.sp.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.sp.beans.Student;
+
+public class Main {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("/in/sp/resources/applicationContext.xml");
+		Student std = (Student) context.getBean("stdId");
+		std.display();
+	}
+}
